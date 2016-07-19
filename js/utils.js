0b0f9893cf338ebe5429577340176bdc208ef6dc
Manolo Bevia <manolo.bevia@cornell.edu>
2016-07-19 12:38:07 -0400

    Add navigation Selected menu hightlighted

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
 
 
+// Navigation Selected menu hightlighted
+
+      var loc = window.location.href; // returns the full URL
+
+      console.log ("location" + log);
+      if(/who-we-are/.test(loc)) {
+        $('#who-we-are').addClass('active');
+      }
 
 });
 
