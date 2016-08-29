44c00d3ff66af3c3d48f79c77e4e9ee37e7ba328
Manolo Bevia <manolo.bevia@gmail.com>
2016-08-29 16:00:43 -0400

    Remove query and bootstrap js library since are duplicated

--- html.tpl.php
+++ html.tpl.php
@@ -59,11 +59,6 @@ $theme_path = drupal_get_path('theme',$GLOBALS['theme']) . '/';
     <?php print $page; ?>
     <?php print $page_bottom; ?>
 
-
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-
-    <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
     <script type="text/javascript" src="/<?php print $theme_path; ?>js/utils.js"></script>
     
 
